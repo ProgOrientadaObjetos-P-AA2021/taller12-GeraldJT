f50ff75c6dcdd2cabd52cf0f13c603443265cd21
¨Gerald¨ <geraldjt.gmail.com>
2021-07-14 16:52:38 -0500

    Taller Completo

--- TallerJUnit/src/paquete1/Estudiante.java
+++ TallerJUnit/src/paquete1/Estudiante.java
@@ -2,7 +2,7 @@
 package paquete1;
 
 public class Estudiante {
-    // 1.  Declarar
+     // 1.  Declarar
     // se declaran datos o atributos con visibilidad protegido
     // # nombresEstudiante: Cadena
     protected String nombresEstudiante;
@@ -12,6 +12,48 @@ public class Estudiante {
     protected String identificacionEstudiante;
     // # edadEstudiante: Entero
     protected int edadEstudiante;
-    
+
+    public void establecerNombresEstudiante(String nE) {
+        nombresEstudiante = nE;
+    }
+
+    public String obtenerNombresEstudiante() {
+        return nombresEstudiante;
+    }
+
+    public void establecerApellidoEstudiante(String aE) {
+        apellidosEstudiante = aE;
+    }
+
+    public String obtenerApellidoEstudiante() {
+        return apellidosEstudiante;
+    }
+
+    public void establecerIdentificacionEstudiante(String id) {
+        identificacionEstudiante = id;
+    }
+
+    public String obtenerIdentificacionEstudiante() {
+        return identificacionEstudiante;
+    }
+
+    public void establecerEdadEstudiante(int e) {
+        edadEstudiante = e;
+    }
+
+    public int obtenerEdadEstudiante() {
+        return edadEstudiante;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format("Nombre Estudiante: %s %s\nCédula: %s",
+                obtenerNombresEstudiante(),
+                obtenerApellidoEstudiante(),
+                obtenerIdentificacionEstudiante());
+        return cadena;
+    }
 
 }
+
+
